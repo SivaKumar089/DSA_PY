13fb0bc40bb672acb52de78aa63dcd2cf1091234
Sivakumar089 <kumarsiva200426@gmail.com>
2025-09-22 22:24:51 +0530

    day 15 learn dsa

---
+++ day-15/Quick Sort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr  # Base case
+    
+    pivot = arr[-1]  # Choose last element as pivot
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# Test
+arr = [38, 27, 43, 3, 9, 82, 10]
+print("Quick Sort:", quick_sort(arr))
